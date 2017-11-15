9b1d6a1d797e2a7c7b473a6100a0a27464fc846e
Mahesh Kalyankar <mahesh.kalyankar@figmd.in>
2017-11-15 10:27:04 +0530

    changed in example.jsx

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -10,7 +10,10 @@ var React = require('react');
 
 var Example = (props) =>{
     return (
+        <div>
             <h3>Example Component !</h3>
+            <p> Welcome to React </p>
+            </div>
     )
 };
 
