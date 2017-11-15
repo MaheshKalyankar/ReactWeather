a64b604e7b4395dd724942ae6111aabb7f755d2d
Mahesh Kalyankar <mahesh.kalyankar@figmd.in>
2017-11-15 14:44:54 +0530

    add foundation and jquery

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-    console.log('Servere is started on localhost:3000');
+app.listen(9000, function () {
+    console.log('Servere is started on localhost:9000');
 })
